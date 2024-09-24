7479a53f97760e793669a8009462539913a19194
Kroese <kroese@users.noreply.github.com>
2024-09-24 18:38:19 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-info () { printf "%b%s%b" "\E[1;34m❯ \E[1;36m" "${1:-}" "\E[0m\n"; }
-error () { printf "%b%s%b" "\E[1;31m❯ " "ERROR: ${1:-}" "\E[0m\n" >&2; }
-warn () { printf "%b%s%b" "\E[1;31m❯ " "Warning: ${1:-}" "\E[0m\n" >&2; }
-
-trap 'error "Status $? while: $BASH_COMMAND (line $LINENO/$BASH_LINENO)"' ERR
-
-[ ! -f "/run/entry.sh" ] && error "Script must run inside Docker container!" && exit 11
-[ "$(id -u)" -ne "0" ] && error "Script must be executed with root privileges." && exit 12
-
-echo "❯ Starting Umbrel for Docker v$(</run/version)..."
-echo "❯ For support visit https://github.com/dockur/umbrel/issues"
-
 if [ ! -S /var/run/docker.sock ]; then
-  error "Docker socket is missing?" && exit  13
+  echo "ERROR: Docker socket is missing? Please bind /var/run/docker.sock in your compose file." && exit  13
 fi
 
 # Create directories
 mkdir -p /images
-mkdir -p /data/tor
 mkdir -p /data/umbrel-os/var
 
 if [ ! -d /data/umbrel-os/home ]; then
