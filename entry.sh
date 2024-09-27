08d4861034d97af1fedad315e329660b3c9c1790
Kroese <kroese@users.noreply.github.com>
2024-09-27 13:34:10 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -31,11 +31,19 @@ fi
 
 mount=$(echo "$resp" | jq -r '.[0].Mounts[] | select(.Destination == "/data").Source')
 
-if [ -z "$mount" ] || [[ "$mount" == "null" ]]; then
+if [ -z "$mount" ] || [[ "$mount" == "null" ]] || [ ! -d /data ]; then
   echo "ERROR: You did not bind the /data folder!" && exit 18
 fi
 
+# Create directories
 mkdir -p "/images"
+
+# Mirror external folder to local filesystem
+if [[ "$mount" != "/data" ]]; then
+  mkdir -p "$mount"
+  rm -rf "$mount"
+  ln -s /data "$mount"
+fi
 
 trap "pkill -SIGINT -f umbreld; while pgrep umbreld >/dev/null; do sleep 1; done" SIGINT SIGTERM
 
