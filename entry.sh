9d14a7858c6491433a7c5c689358d35dd4cc0a15
Kroese <kroese@users.noreply.github.com>
2025-03-03 20:49:55 +0100

    fix: Tor data permissions

--- entry.sh
+++ entry.sh
@@ -50,9 +50,6 @@ if [ -z "$mount" ] || [[ "$mount" == "null" ]] || [ ! -d "/data" ]; then
   error "You did not bind the /data folder!" && exit 18
 fi
 
-# Create directories
-mkdir -p "/images"
-
 # Convert Windows paths to Linux path
 if [[ "$mount" == *":\\"* ]]; then
   mount="${mount,,}"
@@ -70,6 +67,13 @@ if [[ "$mount" != "/data" ]]; then
   rm -rf "$mount"
   ln -s /data "$mount"
 fi
+
+# Create directories
+mkdir -p "/images"
+mkdir -p "$mount/tor/data"
+chmod 700 "$mount/tor/data"
+chmod -R 700 "$mount/tor/data/*"
+chown -R 1000:1000 "$mount/tor"
 
 trap - ERR
 cd /opt/umbreld
